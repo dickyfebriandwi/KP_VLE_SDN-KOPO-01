7627853a6867b7f48b08754105bf425192822562
Dicky Febrian Dwiputra <febriandwiputra@gmail.com>
2021-09-09 23:36:13 +0700

    Registration

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/KP/KP2021_VLE_SDN-KOPO-01';
+$config['base_url'] = 'http://localhost/KP_VLE_SDN-KOPO-01';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -14,6 +14,7 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                 </div>
+                                <?= $this->session->flashdata('message'); ?>
                                 <form class="user">
                                     <div class="form-group">
                                         <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Masukan alamat Email...">
@@ -30,7 +31,7 @@
                                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="register.html">Create an Account!</a>
+                                    <a class="small" href="<?= base_url('auth/registration') ?>">Create an Account!</a>
                                 </div>
                             </div>
                         </div>
