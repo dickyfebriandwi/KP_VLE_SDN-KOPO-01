173066a1f3e127f4764cc1f7a7ec6f8a826d6bee
Dicky Febrian Dwiputra <febriandwiputra@gmail.com>
2021-09-24 02:18:48 +0700

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,35 +12,50 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Admin
-    </div>
+    <!-- Query Menu -->
+    <?php
+    $role_id = $this->session->userdata['role_id'];
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-columns"></i>
-            <span>Dashboard</span></a>
-    </li>
+    $queryMenu =    "SELECT `user_menu`.`id` , `menu`
+                    FROM `user_menu` 
+                    JOIN `user_access_menu` 
+                    ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC
+                    ";
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <!-- Looping Menu -->
+    <?php foreach ($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
+        <!-- Sub Menu -->
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "   SELECT *
+                            FROM `user_sub_menu` 
+                            JOIN `user_menu` ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                            WHERE `user_sub_menu`.`menu_id` = $menuId
+                            AND `user_sub_menu`.`is_active` = 1
+            ";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+        <?php foreach ($subMenu as $sm) : ?>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                    <i class="<?= $sm['icon'] ?>"></i>
+                    <span><?= $sm['title'] ?></span></a>
+            </li>
+        <?php endforeach; ?>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-address-card"></i>
-            <span>Profilku</span></a>
-    </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+    <?php endforeach; ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
