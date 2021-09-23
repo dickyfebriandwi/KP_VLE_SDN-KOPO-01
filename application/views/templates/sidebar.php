000b43ca51ffe8682dd46b831d45d23a3b27cb8c
Dicky Febrian Dwiputra <febriandwiputra@gmail.com>
2021-09-24 01:18:15 +0700

    User

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -3,76 +3,49 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon">
-        <i class=" fas fa-solid fa-graduation-cap"></i>
-    </div>
-    <div class="sidebar-brand-text mx-2">ELITE SCHOOL</div>
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-graduation-cap"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">VLE SDN Kopo 01 </div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- QUERY MENU -->
-    <?php
-        $role_id = $this->session->userdata('role_id');
-        $queryMenu = "SELECT `user_menu`.`id`, `menu`
-                        FROM `user_menu` JOIN `user_access_menu`
-                        ON `user_menu`.`id` = `user_access_menu`.`menu_id`
-                        WHERE `user_access_menu`.`role_id` = $role_id
-                        ORDER BY `user_access_menu`.`menu_id` ASC                
-                    ";
-        $menu = $this->db->query($queryMenu)->result_array();
-    ?>
-    <!-- LOOPING MENU -->
-    <?php foreach ($menu  as $m) : ?>
-        <div class="sidebar-heading">
-            <?= $m['menu']; ?>
-        </div>
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Admin
+    </div>
 
-    <!-- SUB MENU -->
-        <?php
-            $menuId = $m['id'];
-            $querySubmenu =  "SELECT *
-                                FROM `user_submenu` JOIN `user_menu`
-                                ON `user_submenu`.`menu_id` = `user_menu`.`id`
-                                WHERE `user_submenu`.`menu_id` = $menuId
-                                AND `user_submenu`.`is_active` = 1                 
-                            ";
-            $subMenu = $this->db->query($querySubmenu)->result_array();                
-        ?>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-columns"></i>
+            <span>Dashboard</span></a>
+    </li>
 
-        <?php foreach($subMenu as $sm) : ?>
-        <?php if($title == $sm['title']) : ?>
-            <li class="nav-item active">
-                <?php else : ?>
-            <li class="nav-item">
-                
-                <?php endif; ?>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
-                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
-                <i class="<?= $sm['icon']; ?>"></i>
-                <span><?= $sm['title']; ?></span></a>
-            </li>
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        User
+    </div>
 
-        <?php endforeach; ?>
-    
-    <?php endforeach; ?>
-    
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-fw fa-address-card"></i>
+            <span>Profilku</span></a>
+    </li>
 
-            <!-- Nav Item - Log out -->
-            <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Log out</span></a>
-            </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
+</ul>
 <!-- End of Sidebar -->
