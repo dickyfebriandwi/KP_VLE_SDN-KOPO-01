cd8948f2c8332c198c3e7aa7c29a37411c981f6e
Dicky Febrian Dwiputra <febriandwiputra@gmail.com>
2021-10-25 21:36:14 +0700

    Update akun.php

--- application/views/admin/akun.php
+++ application/views/admin/akun.php
@@ -3,7 +3,14 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
-
+    <div class="row">
+        <div class="col-2">
+            <a href="<?= base_url(); ?>admin/akun_guru" class="btn btn-primary mb-3">Lihat Tabel Guru</a>
+        </div>
+        <div class="col-2">
+            <a href="<?= base_url(); ?>admin/akun_siswa" class="btn btn-primary mb-3">Lihat Tabel Siswa</a>
+        </div>
+    </div>
 
 </div>
 
